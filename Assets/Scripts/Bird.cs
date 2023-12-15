d77c2ed9019507fc6c64d7e2e83044e78a0e1d93
Ethan Jimenez <723068@student.dusd.net>
2023-12-14 20:55:57 -0800

    I am working on repeating backgrounds for my game
    For Flappy Bird, I finished the game controller and now working on repeating backgrounds

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -34,6 +34,6 @@ public class bird : MonoBehaviour
     {
         isDead = true;
         anim.SetTrigger("Die");
-        GameControl.instance.BirdDied();
+        GameControl.instance.ToString();
     }
 }
